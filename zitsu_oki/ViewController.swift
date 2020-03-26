4cc1b055785be3cc9db686edc8fe7234a959e8ae
kage1029 <ichika789rider@gmail.com>
2020-03-26 13:40:31 +0900

    Initial Commit

---
+++ zitsu_oki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zitsu_oki
+//
+//  Created by 景山莉穂 on 2020/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ zitsu_okiTests/zitsu_okiTests.swift
@@ -0,0 +1,33 @@
+//
+//  zitsu_okiTests.swift
+//  zitsu_okiTests
+//
+//  Created by 景山莉穂 on 2020/03/26.
+//
+
+import XCTest
+@testable import zitsu_oki
+
+class zitsu_okiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
