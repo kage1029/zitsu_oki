cd0624d1e23af984c93893f3f1b62964a7d819f8
kage1029 <ichika789rider@gmail.com>
2020-03-27 17:20:57 +0900

    Aggange layouts and add textfield

--- zitsu_oki/ViewController.swift
+++ zitsu_oki/ViewController.swift
@@ -23,6 +23,24 @@ class ViewController: UIViewController {
     
     // 起きたボタンを押すと実行する動作
     @IBAction func wakeupBotton(_ sender: UIButton) {
+                
+        hourLabel.text = "時"
+        minutesLabel.text = "分"
+        secondLabel.text = "秒"
+    }
+    
+    
+    // ここからお明日起きる時間Viewの操作
+    // 起きる時間を入力するテキストフィールド
+    @IBOutlet weak var wakeupPlanTextField: UITextField!
+    
+    // 明日起きる時間が表示されるラベル
+    
+    @IBOutlet weak var hourLabel2: UILabel!
+    @IBOutlet weak var minutesLabel2: UILabel!
+    
+    // 明日起きる時間を設定ボタンを押すと実行する動作
+    @IBAction func wakeupPlanBotton(_ sender: UIButton) {
         //現在の日付を取得
         let date:Date = Date()
                 
@@ -32,21 +50,15 @@ class ViewController: UIViewController {
                 
         //日付をStringに変換する
         let sDate = format.string(from: date)
-                
         print(sDate)
-    }
-    
-    
-    // ここからお明日起きる時間Viewの操作
-    // 起きる時間を入力するテキストフィールド
-    @IBOutlet weak var wakeupPlanTextField: UITextField!
-    
-    // 明日起きる時間を設定ボタンを押すと実行する動作
-    @IBAction func wakeupPlanBotton(_ sender: UIButton) {
-        print("aaa")
+        
+        // 入力された時間を取得
         let wakeUpPlanTime = Int(self.wakeupPlanTextField.text ?? "")
         
         print(wakeUpPlanTime)
+        
+        hourLabel2.text = "時2"
+        minutesLabel2.text = "分2"
     }
     
     
