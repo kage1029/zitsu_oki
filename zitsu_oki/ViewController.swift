2cbd04d33cffe22c71a9d9cc468e70ab353f2728
kage1029 <ichika789rider@gmail.com>
2020-03-29 03:30:17 +0900

    delete lavel and textfield for layout

--- zitsu_oki/ViewController.swift
+++ zitsu_oki/ViewController.swift
@@ -7,35 +7,49 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
     // 時間の表示
     @IBOutlet weak var hourLabel: UILabel!
-    @IBOutlet weak var minutesLabel: UILabel!
-    @IBOutlet weak var secondLabel: UILabel!
     
     var timeInterval = TimeInterval()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //現在の日付を取得
+        let date:Date = Date()
+                
+        //日付のフォーマットを指定する。
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm"
+                
+        //日付をStringに変換する
+        let sDate = format.string(from: date)
+        print(sDate)
+        
+        // 入力された時間を取得
+        hourLabel.text = sDate
+        
+        
     }
-    
-    // 起きた時間を入力するテキストフィールド
-    @IBOutlet weak var wakeupTimeTextField: UITextField!
-    
     
     // 起きたボタンを押すと実行する動作
     @IBAction func wakeupBotton(_ sender: UIButton) {
-        print("aaa")
+        //現在の日付を取得
+        let date:Date = Date()
+                
+        //日付のフォーマットを指定する。
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm"
+                
+        //日付をStringに変換する
+        let sDate = format.string(from: date)
+        print(sDate)
         
-        hourLabel.text = "時"
-        minutesLabel.text = "分"
-        secondLabel.text = "秒"
-        
-        let wakeupTime = Int(self.wakeupTimeTextField.text ?? "")
-
-        print(wakeupTime)
+        // 入力された時間を取得
+        hourLabel.text = sDate
         
     }
     
